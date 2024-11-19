9e4e6e3c695f45ff8560fd6346110e1cf6dc6fff
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-19 08:25:07 +0000

    [add] header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+  # ログイン後どこに飛ぶかの設定
+  
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  # ログアウト後どこに飛ぶかの設定
+
   protected
 
   def configure_permitted_parameters
