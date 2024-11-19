c2ab2012a3ec3ccffdf2ea55fd7cf2f89d91fd4d
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-19 15:17:47 +0000

    [add]create機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,29 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
+  end
+
   def index
     @user = current_user
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(current_user.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
