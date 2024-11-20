469a9f3f06cff5f475491cbb4efc518f8f960eef
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-20 07:52:42 +0000

    [update]user/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end
 
   def index
     @user = User.find(current_user.id)
     @book = Book.new
     @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
