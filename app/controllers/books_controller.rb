92b8b62d1d7e14265dc3804374262a6dfcc8877e
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-20 04:46:24 +0000

    [add]book/update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,11 +26,13 @@ class BooksController < ApplicationController
   end
 
   def edit
-
+    @book = Book.find(params[:id])
   end
 
   def update
-
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(params[:id])
   end
 
   def destroy
