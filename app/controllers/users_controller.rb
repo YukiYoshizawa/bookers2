3dd3993a0a2fcaa9dd105959b66dd3b6b144e1dd
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-20 07:00:27 +0000

    [update] user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books
     @book = Book.new
+    @books = @user.books
   end
 
   def edit
   end
 
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @users = User.all
   end
 
 end
